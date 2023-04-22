157d8cb6f26a84d83009f85fb8b90708a608789b
rajithsanjaya <rajith_s@epiclanka.net>
2023-04-22 17:15:44 +0530

    finish basic chat function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Chat-GPT';
+  message:string = ''
+  chatGPT_message:string = ''
+
+private apiUrl = 'https://api.openai.com/v1/chat/completions';
+  constructor(private http: HttpClient) {}
+
+  async createChatCompletion() {
+    const model = 'gpt-3.5-turbo';
+    const messages = [{role: 'user', content: this.message}];
+    const apiKey = environment.API_KEY
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    };
+
+    const body = {
+      model,
+      messages
+    };
+
+    await this.http.post(this.apiUrl, body, {headers}).subscribe((result:any) =>{
+      console.log(result.choices[0].message.content)
+      this.chatGPT_message = result.choices[0].message.content
+    })
+  }
+
 }
+
