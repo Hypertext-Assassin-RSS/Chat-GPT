ab389cfd515923055e490fe5d2117e68b32ea7d3
rajithsanjaya <rajith_s@epiclanka.net>
2023-04-22 18:50:07 +0530

    add array to make chat history visible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,24 @@ import { environment } from "src/environments/environment";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message:string = ''
-  chatGPT_message:string = ''
+  userMessage:string = '';
+  userMessages:any[] = [];
+  chatGPT_message:string = '';
+  chatGPT_messages:any = [];
+  indexes = [0]
+
+
 
 private apiUrl = 'https://api.openai.com/v1/chat/completions';
   constructor(private http: HttpClient) {}
 
   async createChatCompletion() {
     const model = 'gpt-3.5-turbo';
-    const messages = [{role: 'user', content: this.message}];
+    const messages = [{role: 'user', content: this.userMessage}];
     const apiKey = environment.API_KEY
+
+    this.indexes.push(this.indexes.length)
+    this.userMessages.push(this.userMessage)
 
     const headers = {
       'Content-Type': 'application/json',
@@ -32,6 +40,7 @@ private apiUrl = 'https://api.openai.com/v1/chat/completions';
     await this.http.post(this.apiUrl, body, {headers}).subscribe((result:any) =>{
       console.log(result.choices[0].message.content)
       this.chatGPT_message = result.choices[0].message.content
+      this.chatGPT_messages.push(result.choices[0].message.content)
     })
   }
 
